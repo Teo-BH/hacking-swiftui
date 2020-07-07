a8192f69f2605a59bfeeb1b2c41f282c26216ddd
Teobaldo Moura <tmoura@ciandt.com>
2020-07-07 16:29:02 -0300

    Formatting interpolated strings in SwiftUI - SwiftUI Starter Project 11/14

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -18,6 +18,13 @@ struct CheckoutView: View {
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 1
 
+    private var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 *
+            Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
+
     var body: some View {
         Form {
             Section {
@@ -42,7 +49,7 @@ struct CheckoutView: View {
                 }
             }.pickerStyle(SegmentedPickerStyle())
 
-            Section(header: Text("TOTAL: $100")) {
+            Section(header: Text("TOTAL: $\(totalPrice, specifier: "%.2f")")) {
                 Button("Confirm order") {
                     // place the order
                 }
