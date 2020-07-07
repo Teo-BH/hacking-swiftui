9b2ee5a3ca78b8511e377f06955add4c5e64d689
Teobaldo Moura <tmoura@ciandt.com>
2020-07-07 11:26:59 -0300

    Setting up - SwiftUI Started Project 1/14

---
+++ iDine/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Teobaldo Mauro de Moura on 07/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Teobaldo Mauro de Moura on 07/07/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
