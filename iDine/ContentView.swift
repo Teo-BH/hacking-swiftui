44f45a3235ee16d4b2e53829a00bdc769edf495f
Teobaldo Moura <tmoura@ciandt.com>
2020-07-07 11:29:45 -0300

    Building a menu using List - SwiftUI Started Project 2/14

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("My Menu")
+            .listStyle(GroupedListStyle())
+        }
     }
 }
 
