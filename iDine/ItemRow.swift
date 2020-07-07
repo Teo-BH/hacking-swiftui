1b0b8c1c7552a3c33aa2f41638b7ad15d8c7e6f6
Teobaldo Moura <tmoura@ciandt.com>
2020-07-07 14:28:16 -0300

    Displaying a detail screen with NavigationLink - SwiftUI Starter Project 5/14

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -17,26 +17,28 @@ struct ItemRow: View {
     var item: MenuItem
 
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-            VStack(alignment: .leading) {
-                Text(item.name)
-                    .font(.headline)
-                Text("$\(item.price)")
-            }.layoutPriority(1)
+        NavigationLink(destination: ItemDetail(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }.layoutPriority(1)
 
-            Spacer()
+                Spacer()
 
-            ForEach(item.restrictions, id: \.self) { restriction in
-                Text(restriction)
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(5)
-                    .background(Self.colors[restriction, default: .black])
-                    .clipShape(Circle())
-                    .foregroundColor(.white)
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
         }
     }
