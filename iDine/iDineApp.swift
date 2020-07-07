83647714206921dd60c45e4c3da57d05abf8cb5a
Teobaldo Moura <tmoura@ciandt.com>
2020-07-07 14:49:33 -0300

    Observable objects, environment objects, and @Published - SwiftUI Starter Project 6/14

--- iDine/iDineApp.swift
+++ iDine/iDineApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct iDineApp: App {
+    var order = Order()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(order)
         }
     }
 }
